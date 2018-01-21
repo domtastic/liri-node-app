819f94bed00e33c3819b3ae2a67442143c3c70b2
Dominic <gonza362@gmail.com>
2018-01-20 18:35:28 -0700

    is this working?

--- liri.js
+++ liri.js
@@ -1,13 +1,14 @@
-let command = process.argv[2];
+let arrUserInput = process.argv;
+let command = arrUserInput[2];
 const fs = require("fs");
 
 if (command === "my-tweets") {
   // ____ TWITTER TWEETS_______
-  const Twitter = require("twitter");
+  const twitter = require("twitter");
   // ------ link to key.js instead ????? LOADING, BUT NOT WORKING ??????
   // let domTwitterKeys = require("./keys.js");
   // let client = new Twitter(domTwitterKeys);
-  let client = new Twitter({
+  let client = new twitter({
     consumer_key: "etx6VdK6tiTBGtATzYcGcwog0",
     consumer_secret: "KGnLrF0qOcrtXqasFUznS0BrpOw5cFUXviIZ5UPxNnunyyIOUE",
     access_token_key: "954843821078069248-n2pNq374LHkVemZbTC4V7jOuIrQKHOL",
@@ -31,6 +32,26 @@ if (command === "my-tweets") {
   });
 } else if (command === "spotify-this-song") {
   // __________ SPOTIFY _________________
+  // take the User input array and create a string from everything after the 2nd index
+  let song;
+  for (let i = 3; i < arrUserInput.length; i++) {
+    song += arrUserInput[i];
+  }
+  console.log("song: " + song);
+  let spotify = require("node-spotify-api");
+
+  let spotifyCLient = new spotify({
+    id: "12ff9337b04a481a9f1ef068a81347c5",
+    secret: "7ec80c9360ed4bc694f553f8ca7ec599"
+  });
+
+  spotifyClient.search({ type: "track", query: song }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
 } else if (command === "movie-this") {
   // __________ MOVIE _________________
 } else if (command === "do-what-it-says") {
